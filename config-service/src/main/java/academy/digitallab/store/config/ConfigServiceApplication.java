27212df49200b05f2e9946cb0c4c20c84de32b1c
andres-orozco1 <andres-orozco1@hotmail.com>
2021-06-28 17:12:00 -0500

    Agregada config registry-server

--- config-service/src/main/java/academy/digitallab/store/config/ConfigServiceApplication.java
+++ config-service/src/main/java/academy/digitallab/store/config/ConfigServiceApplication.java
@@ -1,6 +1,7 @@
 package academy.digitallab.store.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
